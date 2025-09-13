71b6f243c37a3171561c41a20e4f7326a4556024
Robel <contact.robel@proton.me>
2025-09-14 01:19:36 +0300

    added a new route and new 'comments' field for the blog model

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,77 +1,102 @@
-const router = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const { userExtractor } = require('../utils/middleware')
-const jwt = require('jsonwebtoken')
+const router = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const { userExtractor } = require("../utils/middleware");
+const jwt = require("jsonwebtoken");
 
-router.get('/', async (re, response) => {
-  const blogs = await Blog.find({}).populate('user', '-blogs')
-  response.json(blogs)
-})
+router.get("/", async (re, response) => {
+  const blogs = await Blog.find({}).populate("user", "-blogs");
+  response.json(blogs);
+});
 
-router.post('/', userExtractor, async (request, response) => {
-  const body = request.body
-  const user = request.user
+router.post("/", userExtractor, async (request, response) => {
+  const body = request.body;
+  const user = request.user;
 
   if (!user) {
-    return response.status(400).json({ error: "UserId missing or not valid" })
+    return response.status(400).json({ error: "UserId missing or not valid" });
   }
 
-  request.body.likes = request.body.likes ? request.body.likes : 0
+  request.body.likes = request.body.likes ? request.body.likes : 0;
 
   if (!request.body.title || !request.body.url) {
-    return response.status(400).json({ error: "title and url must be provided" })
+    return response
+      .status(400)
+      .json({ error: "title and url must be provided" });
   }
-  
-  const blog = new Blog({ ...request.body, user: user.id })
 
-  const result = await blog.save()
-  user.blogs.push(result._id)
+  const blog = new Blog({ ...request.body, user: user.id, comments: [] });
 
-  await user.save()
+  const result = await blog.save();
+  user.blogs.push(result._id);
 
-  response.status(201).json(result)
-})
+  await user.save();
 
-router.delete('/:id', userExtractor, async (request, response) => {
-  const user = request.user
+  response.status(201).json(result);
+});
+
+router.delete("/:id", userExtractor, async (request, response) => {
+  const user = request.user;
 
   if (!user) {
-    return res.status(400).json({ error: "UserId missing or not valid" })
+    return res.status(400).json({ error: "UserId missing or not valid" });
   }
 
-  const id = request.params.id
-  const blogToDelete = await Blog.findById(id)
+  const id = request.params.id;
+  const blogToDelete = await Blog.findById(id);
 
   if (!blogToDelete) {
-    return response.status(404).json({ error: "Blog not found"})
+    return response.status(404).json({ error: "Blog not found" });
   }
 
-
-  if (!user.blogs.some(blogId => blogId.toString() === id)) {
-    return response.status(401).json({ error: "unauthorized" })
+  if (!user.blogs.some((blogId) => blogId.toString() === id)) {
+    return response.status(401).json({ error: "unauthorized" });
   }
 
-  await Blog.findByIdAndDelete(id)
+  await Blog.findByIdAndDelete(id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-router.put('/:id', async (request, response) => {
-  const id = request.params.id
+router.put("/:id", async (request, response) => {
+  const id = request.params.id;
 
   if (request.body.likes === undefined) {
-    return response.status(400).json({ error: "'likes' field missing" })
+    return response.status(400).json({ error: "'likes' field missing" });
   }
 
-  const blog = await Blog.findById(id)
+  const blog = await Blog.findById(id);
   if (!blog) {
-    return response.status(404).json({ error: "Blog not found" })
+    return response.status(404).json({ error: "Blog not found" });
   }
-  blog.likes = request.body.likes
-  const updatedBlog = await blog.save()
+  blog.likes = request.body.likes;
+  const updatedBlog = await blog.save();
 
-  response.json(updatedBlog)
-})
+  response.json(updatedBlog);
+});
 
-module.exports = router
+router.post("/:id/comments", userExtractor, async (req, res) => {
+  const user = req.user;
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const id = req.params.id;
+
+  const blog = await Blog.findById(id);
+  if (!blog) {
+    return res.status(404).json({ error: "Blog not found" });
+  }
+
+  if (!req.body.comment) {
+    return res.status(400).json({ error: "'comment' must be provided" });
+  }
+
+  blog.comments.push({ text: req.body.comment });
+
+  const updatedBlog = await blog.save();
+
+  res.status(201).json(updatedBlog);
+});
+
+module.exports = router;
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   url: String,
   likes: Number,
-})
+  comments: [{ text: String }],
+});
 
-blogSchema.set('toJSON', {
+blogSchema.set("toJSON", {
   transform: (doc, ret) => {
-    ret.id = ret._id.toString()
-    delete ret._id
-    delete ret.__v
-    return ret
-  }
-})
+    ret.id = ret._id.toString();
+    ret.comments = ret.comments.map((comment) => ({
+      id: comment._id.toString(),
+      text: comment.text,
+    }));
+    delete ret._id;
+    delete ret.comments._id;
+    delete ret.__v;
+    return ret;
+  },
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-module.exports = Blog
+module.exports = Blog;
